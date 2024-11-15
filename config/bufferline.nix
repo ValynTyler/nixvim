a07877c9ddd29d08db608878b5f53cf98b805266
Valyn Tyler <git@valyntyler.com>
2024-11-15 13:58:27 +0200

    feat: added bufferline mappings to go to nth tab

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -3,4 +3,11 @@
     bufferline.enable = true;
     web-devicons.enable = true;
   };
+
+  keymaps = [
+    { key = "<A-1>"; action = "<cmd>BufferLineGoToBuffer 1<CR>"; }
+    { key = "<A-2>"; action = "<cmd>BufferLineGoToBuffer 2<CR>"; }
+    { key = "<A-3>"; action = "<cmd>BufferLineGoToBuffer 3<CR>"; }
+    { key = "<A-4>"; action = "<cmd>BufferLineGoToBuffer 4<CR>"; }
+  ];
 }
