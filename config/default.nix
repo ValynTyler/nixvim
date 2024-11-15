eea9ca38105af700c7bd6348c5e99d8dfe40e79d
Valyn Tyler <git@valyntyler.com>
2024-11-15 18:08:16 +0200

    update formatting on harpoon keymaps

--- config/default.nix
+++ config/default.nix
@@ -46,10 +46,10 @@
       toggleQuickMenu = "<leader>hh";
       addFile = "<leader>ha";
       navFile = {
-	"1" = "<leader>h1";
-	"2" = "<leader>h2";
-	"3" = "<leader>h3";
-	"4" = "<leader>h4";
+        "1" = "<leader>h1";
+        "2" = "<leader>h2";
+        "3" = "<leader>h3";
+        "4" = "<leader>h4";
       };
       
     };
