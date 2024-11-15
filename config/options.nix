336c57dd41a2df444186bf4b447160ba7527d75a
Valyn Tyler <git@valyntyler.com>
2024-11-15 20:53:51 +0200

    moved options into their own file

---
+++ config/options.nix
@@ -0,0 +1,17 @@
+{
+  globals.mapleader = " ";
+
+  globalOpts = {
+    scrolloff = 12;
+
+    # shiftwidth
+    shiftwidth = 2;
+    tabstop = 2;
+    softtabstop = 2;
+    expandtab = true;
+
+    # numbers
+    nu = true;
+    rnu = true;
+  };
+}
