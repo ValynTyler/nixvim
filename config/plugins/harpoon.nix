d2c3b109bbd937020eec828b6fd72a45c5fa00a5
Valyn Tyler <git@valyntyler.com>
2024-11-15 20:36:53 +0200

    moved harpoon into its own module

---
+++ config/plugins/harpoon.nix
@@ -0,0 +1,15 @@
+{
+  plugins = {
+    harpoon.enable = true;
+    harpoon.keymaps = {
+      toggleQuickMenu = "<leader>hh";
+      addFile = "<leader>ha";
+      navFile = {
+        "1" = "<leader>h1";
+        "2" = "<leader>h2";
+        "3" = "<leader>h3";
+        "4" = "<leader>h4";
+      };
+    };
+  };
+}
