5fa8d36a1e7e9da8fc2abbebf33ed3847cf0dd93
Valyn Tyler <git@valyntyler.com>
2024-11-15 18:35:06 +0200

    feat: added completion plugin

--- config/default.nix
+++ config/default.nix
@@ -66,6 +66,40 @@
       rust_analyzer.installRustc = true;
       rust_analyzer.installCargo = true;
     };
+
+    # completion
+    cmp.enable = true;
+    cmp.autoEnableSources = true;
+    cmp.settings.sources = [
+    { name = "nvim_lsp"; }
+    { name = "path"; }
+    { name = "buffer"; }
+    ];
+    cmp.settings.mapping = {
+      "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      "<C-j>" = "cmp.mapping.select_next_item()";
+      "<C-k>" = "cmp.mapping.select_prev_item()";
+      "<C-e>" = "cmp.mapping.abort()";
+      "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+      "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      "<C-Space>" = "cmp.mapping.complete()";
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+      "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+      "<C-l>" = ''
+        cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' })
+      '';
+      "<C-h>" = ''
+        cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' })
+      '';
+    };
   };
 
   globalOpts = {
