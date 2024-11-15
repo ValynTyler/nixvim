c4392e5c7ed041898ddd8dacb4c05d6eaa770850
Valyn Tyler <git@valyntyler.com>
2024-11-15 19:45:27 +0200

    feat: added `v` move line keymaps

--- config/default.nix
+++ config/default.nix
@@ -165,5 +165,17 @@
       key = "<leader>d";
       action = "\"_d";
     }
+
+    # Move currently selected line with J and K
+    {
+      mode = "v";
+      key = "J";
+      action = ":m '>+1<CR>gv=gv";
+    }
+    {
+      mode = "v";
+      key = "K";
+      action = ":m '<-2<CR>gv=gv";
+    }
   ];
 }
