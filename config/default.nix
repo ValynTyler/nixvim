bbf8e1ac6c449921c05b9c2f35e9588a7cf2bb67
Valyn Tyler <git@valyntyler.com>
2024-11-15 15:13:59 +0200

    feat: added and configured treesitter

--- config/default.nix
+++ config/default.nix
@@ -14,6 +14,29 @@
     # undotree
     undotree.enable = true;
 
+    # treesitter
+    treesitter.enable = true;
+    treesitter.settings = {
+      auto_install = true;
+      ensure_installed = [
+	"rust"
+	"go"
+	"nix"
+	"lua"
+	"vim"
+	"vimdoc"
+	"query"
+	"toml"
+	"yaml"
+	"markdown"
+	"markdown_inline"
+      ];
+
+      # highlight
+      hightlight.enable = true;
+      hightlight.additional_vim_regex_highlighting = false;
+    };
+
     # telescope
     telescope.enable = true;
 
