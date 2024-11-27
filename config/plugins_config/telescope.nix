88982b4e02b44ff284c2f58f11fa5c8ffd554504
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-27 11:45:21 +0200

    cleaned up telescope.nix

--- config/plugins_config/telescope.nix
+++ config/plugins_config/telescope.nix
@@ -1,27 +1,10 @@
 {
-  plugins.telescope.enable = true;
-
-  keymaps = [
-    # Fuzzyfind all files
-    {
-      mode = "n";
-      key = "<leader>ff";
-      action = "<cmd>Telescope find_files<CR>";
-      options = {
-        desc = "Open Telescope";
-      };
-    }
-    # Fuzzyfind git (project) files
-    {
-      mode = "n";
-      key = "<leader>fp";
-      action = "<cmd>Telescope git_files<CR>";
-    }
-    # Fuzzyfind arbitrary sequence
-    {
-      mode = "n";
-      key = "<leader>fs";
-      action = "<cmd>Telescope live_grep<CR>";
-    }
-  ];
+  plugins.telescope = {
+    enable = true;
+    keymaps = {
+      "<leader>ff" = "find_files";
+      "<leader>fp" = "git_files";
+      "<leader>fs" = "live_grep";
+    };
+  };
 }
