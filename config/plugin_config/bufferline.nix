25af69b40dc2c92bb5dec20dcdac7ce33f8c2816
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-21 21:02:28 +0200

    feat: can now pick buffers from bufferline with alt+0-9

--- config/plugin_config/bufferline.nix
+++ config/plugin_config/bufferline.nix
@@ -6,5 +6,11 @@
     { key = "<A-2>"; action = "<cmd>BufferLineGoToBuffer 2<CR>"; }
     { key = "<A-3>"; action = "<cmd>BufferLineGoToBuffer 3<CR>"; }
     { key = "<A-4>"; action = "<cmd>BufferLineGoToBuffer 4<CR>"; }
+    { key = "<A-5>"; action = "<cmd>BufferLineGoToBuffer 5<CR>"; }
+    { key = "<A-6>"; action = "<cmd>BufferLineGoToBuffer 6<CR>"; }
+    { key = "<A-7>"; action = "<cmd>BufferLineGoToBuffer 7<CR>"; }
+    { key = "<A-8>"; action = "<cmd>BufferLineGoToBuffer 8<CR>"; }
+    { key = "<A-9>"; action = "<cmd>BufferLineGoToBuffer 9<CR>"; }
+    { key = "<A-0>"; action = "<cmd>BufferLineGoToBuffer 10<CR>"; }
   ];
 }
