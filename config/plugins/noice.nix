0294fbcccdf212a3d0ff2fd88d3ef40d0e4fccf8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-28 23:27:50 +0200

    feat: added keymap for clearing notifications in noice

--- config/plugins/noice.nix
+++ config/plugins/noice.nix
@@ -1,3 +1,10 @@
 {
   plugins.noice.enable = true;
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ca";
+      action = "<cmd>lua require('notify').dismiss({ silent = true, pending = true })<CR>";
+    }
+  ];
 }
