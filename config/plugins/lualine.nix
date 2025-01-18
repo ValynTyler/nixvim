92a3a979aa2e36d437cfdaa01a28c0e38d98bbce
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-18 20:36:10 +0200

    fix: lualine recording eval warning

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,14 +1,14 @@
 {
   plugins.lualine.enable = true;
-  plugins.lualine.sections.lualine_x = 
+  plugins.lualine.settings.sections.lualine_x = 
   let
     bright_orange = "#fe8019";
   in
   [
     {
-      name.__raw = "require('noice').api.statusline.mode.get";
+      "__unkeyed.1".__raw = "require('noice').api.statusline.mode.get";
       color = { fg = bright_orange ; };
-      extraConfig.cond.__raw = "require('noice').api.statusline.mode.has";
+      cond.__raw = "require('noice').api.statusline.mode.has";
     }
   ];
 }
