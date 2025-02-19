70690f2d95e7de7da37bee1acab29b4a09d63fa5
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-19 19:41:02 +0200

    feat: re-enabled oil

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,19 +1,5 @@
 {
   plugins.oil = {
     enable = true;
-    settings = {
-      default_file_explorer = false;
-      constrain_cursor = "editable";
-      delete_to_trash = true;
-      view_options.show_hidden = true;
-    };
   };
-  keymaps = [
-    # Open Oil
-    {
-      mode = "n";
-      key = "<leader>o";
-      action = "<CMD>Oil %:h<CR>";
-    }
-  ];
 }
